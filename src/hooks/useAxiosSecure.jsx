cfc518a51b02327fbea1c35bf272f29265a01091
laizu01993 <laizuaktar6@gmail.com>
2025-12-14 21:58:52 +0600

    implement axiosSecure request interceptor for JWT authorization

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,6 +4,18 @@ const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+
+    axiosSecure.interceptors.request.use(function(config) {
+
+        const token = localStorage.getItem('access-token');
+        console.log('request stopped by interceptors', token)
+        config.headers.Authorization = `Bearer ${token}`;
+
+        return config;
+    }, function (error) {
+        return Promise.reject(error);
+    })
+
     return axiosSecure;
 };
 
