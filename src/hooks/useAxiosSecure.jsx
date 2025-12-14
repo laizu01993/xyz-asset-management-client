8766d01ae12b6042d3d407803325ac21bc2c67c8
laizu01993 <laizuaktar6@gmail.com>
2025-12-15 02:01:01 +0600

    centralize auth error handling with axiosSecure response interceptor

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,10 +1,17 @@
 import axios from "axios";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
 
 const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiosSecure = () => {
+    
+    const navigate = useNavigate();
+    const {logOut} = useContext(AuthContext);
 
+    // request interceptor to add authorization header for every secure call to the API
     axiosSecure.interceptors.request.use(function(config) {
 
         const token = localStorage.getItem('access-token');
@@ -14,6 +21,21 @@ const useAxiosSecure = () => {
         return config;
     }, function (error) {
         return Promise.reject(error);
+    });
+
+    // response interceptor (401 and 403 status)
+    axiosSecure.interceptors.response.use(function(response) {
+        return response;
+    }, async (error) => {
+        const status = error.response.status;
+        console.log('status error in the interceptor', status);
+
+        // for 401 or 403 logOut the user and move the user to the login page
+        if(status === 401 || status === 403) {
+            await logOut();
+            navigate('/login')
+        }
+        return Promise.reject(error)
     })
 
     return axiosSecure;
