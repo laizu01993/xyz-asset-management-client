c64262ad2f558acb590cfa03793eda70a8dadd93
laizu01993 <laizuaktar6@gmail.com>
2026-01-25 02:05:01 +0600

    fix: update useUserData to wait for auth state and token before fetching user data

--- src/hooks/useUserData.jsx
+++ src/hooks/useUserData.jsx
@@ -5,12 +5,12 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useUserData = () => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
   const axiosSecure = useAxiosSecure();
 
   const { data: userData, isLoading } = useQuery({
     queryKey: ["userData", user?.email],
-    enabled: !!user?.email,
+    enabled: !!user?.email && !loading,
     queryFn: async () => {
       const res = await axiosSecure.get(`/users/${user.email}`);
       return res.data;   
